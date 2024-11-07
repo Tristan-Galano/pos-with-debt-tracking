596504b4965f20a393ea2312a479ac5b07d59b61
TristanG <Tristan.Galano22@gmail.com>
2024-11-07 16:59:39 +0800

    Insert transaction fucntion

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template,redirect, request, session, url_for
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 from config import Config
 
@@ -17,15 +18,17 @@ app.app_context().push()
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
 @app.route("/login", methods=["POST"])
 def login():    
+
     username = request.form["username"]
     password = request.form["password"]
     user = models.User.query.filter_by(username=username).first()
     if username == user.username and password==user.password:
-        session['id'] =user.id
+        session['id'] = user.id
         session['username'] = username
         session['admin'] = user.admin
         return redirect('/cashier')
@@ -34,6 +37,7 @@ def login():
 
 @app.route("/sign-up")
 def create_user():
+
     if session.get('admin'):
         if session['admin'] == True:
             return render_template("signup.html")
@@ -42,18 +46,43 @@ def create_user():
     
 @app.route("/cashier")
 def cashier():
+      
       if session.get('username'):
+          
         debtors = models.Debtor.query.all()
-        
         return render_template('cashier.html', debtors = debtors)
+    
       else:
           return redirect('/')
       
-
 @app.route('/tansaction', methods=['POST', 'GET'])
 def tansaction():
-    amount = request.form["amount"]
-    print(amount)
-    return jsonify({'result':'ok'}), 200
-
-        
+    
+    fullname = request.form['namelist']
+    amount = request.form['amount']
+    debtor_id = None
+    user_id = session['id']
+    trans_type = "sale"
+    status = "Completed"
+    updated_by = user_id
+    
+    if fullname:
+        trans_type="credit" 
+        debtor = models.Debtor.query.filter(models.Debtor.name+ ' ' + models.Debtor.lastname==fullname).first()
+        debtor_id = debtor.debtor_id
+    
+    transaction = models.Transaction(
+        id = user_id,
+        debtor_id = debtor_id,
+        amount = amount,
+        name = trans_type,
+        status = status,
+        updated_by = updated_by   
+    )
+    
+    if amount:
+        db.session.add(transaction)
+        db.session.commit()
+        return jsonify({'result':'ok'}), 200
+    
+    return jsonify({'result':'not ok'}), 200
--- static/js/script.js
+++ static/js/script.js
@@ -1,51 +1,60 @@
 // JavaScript code to make the calculator work
 // Get all the calculator buttons
-const buttons = document.querySelectorAll('.calc-button');
-const display = document.getElementById('display');
-const disnumberInputplay = document.getElementById('display');
+const buttons = document.querySelectorAll(".calc-button");
+const display = document.getElementById("display");
+const debtor_name = document.getElementById("namelist");
 
-let currentInput = '';
+let currentInput = "";
+let flag = true;
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const value = button.getAttribute('data-value');
-        
-        if (value === 'C') {
-            currentInput = '';
-            display.value = '';
-        } else if (value === '=') {
-            try {
-                currentInput = eval(currentInput).toString();
-                display.value = currentInput;
-            } catch (e) {
-                display.value = 'Error';
-                currentInput = '';
-            }
-        } else {
-            currentInput += value;
-            display.value = currentInput;
-        }
-    });
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.getAttribute("data-value");
+
+    if (value === "C") {
+      currentInput = "";
+      display.value = "";
+    } else if (value === "=") {
+      try {
+        currentInput = eval(currentInput).toString();
+        display.value = currentInput;
+        flag = false;
+      } catch (e) {
+        display.value = "Error";
+        currentInput = "";
+        flag = true;
+      }
+    } else {
+      currentInput += value;
+      display.value = currentInput;
+    }
+  });
 });
 
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevents default submission
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevents default submission
 
-    const formData = new FormData(this);
+  const formData = new FormData(this);
 
-    fetch('http://127.0.0.1:5000//tansaction', {
-      method: 'POST',
-      body: formData
+  if (flag != true) {
+    fetch("http://127.0.0.1:5000//tansaction", {
+      method: "POST",
+      body: formData,
     })
-    .then(response => response.json())
-    .then(data => {
-      // Process the response data
-      console.log('Success:', data);
-      // You can update the page content here without reloading
-      display.value = '';
-      disnumberInputplay.value = '';
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-  });
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the response data
+        console.log("Success:", data);
+        display.value = "";
+        debtor_name.value = "";
+        currentInput = "";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
+    display.value = "Error";
+    debtor_name.value = "";
+    currentInput = "";
+  }
+});
